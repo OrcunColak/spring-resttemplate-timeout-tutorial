b16420a4efa7cee790a9455470704a55de181cb3
Orçun Çolak <orcun.colak@hazelcast.com>
2024-11-26 08:44:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Read me 
+
+
+The original idea is from  
+https://dzone.com/articles/timeout-in-spring-boot-with-restclient-webclient-a
+
+#
---
+++ src/test/java/com/colak/springtutorial/service/resttemplate/RestTemplateServiceTest.java
@@ -0,0 +1,40 @@
+package com.colak.springtutorial.service.resttemplate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RestTemplateServiceTest {
+
+    @Autowired
+    private RestTemplateService restTemplateService;
+
+    @LocalServerPort
+    int randomPort;
+
+    @Test
+    void getWithExchange() {
+        String url = "http://localhost:" + randomPort + "/api/v1/quote/getquote";
+        ResponseEntity<String> responseEntity = restTemplateService.getWithExchange(url, String.class);
+        String page = responseEntity.getBody();
+        assertThat(page).isEqualTo("quote");
+    }
+
+    @Test
+    void getListWithExchange() {
+        String url = "http://localhost:" + randomPort + "/api/v1/quote/getquotelist";
+        ResponseEntity<List<String>> responseEntity = restTemplateService.getListWithExchange(url);
+        List<String> result = responseEntity.getBody();
+        List<String> expected = List.of("quote1", "quote1");
+        Assertions.assertEquals(expected, result);
+    }
+
+}
